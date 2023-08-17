9363200ee9021dbc17aa7e4464880ff7aa048c73
ninoslat1 <benedict.ellnina@gmail.com>
2023-08-17 11:02:04 +0700

    fix logout function

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -8,7 +8,7 @@ export default function Topbar() {
 
     const handleLogout = () => {
         dispatch({type:"LOGOUT"})
-        window.location.replace("/login")
+        window.location.replace("/")
     }
 
   return (
